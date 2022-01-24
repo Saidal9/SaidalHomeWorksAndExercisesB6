06fe468b9fff117137bfb99359c6fcf48a646d20
Saidal <Saidal@LAPTOP-M4QQOIPJ.myfiosgateway.com>
2022-01-24 11:07:32 -0500

    Updated, thank you.

---
+++ src/day10And11MethodsHomework/Day10Question4.java
@@ -0,0 +1,29 @@
+package day10And11MethodsHomework;
+
+public class Day10Question4 {
+	public static void main(String[] args) { 
+		
+		
+		//Write a method that takes 2 parameters. In the method write the following logic.
+		//Using do -while loop, loop through the numbers provided in the parameter. Loop should start from minNum and stop at maxNum. Print each number
+
+		
+			
+			getDoWhileLoopResult(20, 90);
+			
+		}
+												//Two Parameters
+		public static void getDoWhileLoopResult(int minNum, int maxNum) {
+							//Using Do=While Loop
+			do {
+				//Do keep adding to minNum (++minNum)
+					System.out.println(maxNum);
+					minNum++;
+					
+					// while minNum has not equaled to maxNum (which is 90)
+				} while (minNum>=90);
+				
+		}
+	}
+						//Wrong Answer please fix. Much Thanks.
+
